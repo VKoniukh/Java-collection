c86c31500832d6a682839b467c03c60f4f6eecdb
Volodymyr <konuhvladimir1@gmail.com>
2021-07-09 01:03:17 +0300

    35

--- src/main/java/com/epam/rd/java/basic/practice2/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice2/Demo.java
@@ -5,14 +5,42 @@ import java.util.Iterator;
 public class Demo {
 
     public static void main(String[] args) {
-        ArrayImpl objects = new ArrayImpl(2);
-//        System.out.println(objects.size);
-//        System.out.println(objects.index);
-        objects.add("A");
-        objects.add("b");
-        objects.add("c");
-        objects.add(null);
+        List objects = new ListImpl();
+        /* Start with the empty list */
+//        DLL dll = new DLL();
+//
+//        // Insert 6. So linked list becomes 6->NULL
+//        dll.append(6);
+//
+//        // Insert 7 at the beginning. So
+//        .. linked list becomes 7->6->NULL
+//        dll.push(7);
+//
+//        // Insert 1 at the beginning. So
+//        // linked list becomes 1->7->6->NULL
+//        dll.push(1);
+//
+//        // Insert 4 at the end. So linked
+//        // list becomes 1->7->6->4->NULL
+//        dll.append(4);
+//
+//        // Insert 8, after 7. So linked
+//        // list becomes 1->7->8->6->4->NULL
+//        dll.InsertAfter(dll.head.next, 8);
+//
+//        System.out.println("Created DLL is: ");
+//        dll.printlist(dll.head);
+        objects.addFirst("A");
+        objects.addFirst("b");
+        objects.addLast("c");
+        System.out.println(objects.size());
+        System.out.println( objects.getFirst());
+        System.out.println( objects.getLast());
         System.out.println(objects.toString());
+        System.out.println(objects.search("b"));
+        objects.remove("b");
+        System.out.println(objects.toString());
+
         Iterator<Object> iterator = objects.iterator();
             while (iterator.hasNext()) {
                 System.out.println(iterator.next());
